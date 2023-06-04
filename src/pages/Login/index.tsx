a8972dfe3467921cbd1de47e572e40a07070d8a9
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2023-06-04 00:03:18 -0300

    iniciando estrutura da tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,13 @@
-import { Button } from "@mui/material";
+import React, { useState } from 'react';
+import { Button, TextField } from "@mui/material";
 import useAuth from "../../router/validations/Auth/hook/useAuth";
 import { useNavigate } from "react-router-dom";
+import './index.css'
 
 export default function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     const {handleLogin} = useAuth()
     const navigate = useNavigate()
     function handleLoginInButton(){
@@ -11,6 +16,27 @@ export default function Login(){
     }
     return <>
     <h1>ISSO È UM TESTE :D</h1>
-    <Button color="secondary" variant="contained" onClick={()=> handleLoginInButton()} >Entrar</Button>
+    <form id="login-form">    
+        <TextField
+            className='login-element'
+            type='text'
+            id="usernameId"
+            label="Nome do Usuário"
+            value={username}
+            onChange={(event) => {setUsername(event.target.value)}}    
+        /> 
+        <TextField
+            className='login-element'
+            type='text'
+            id="passwordId"
+            label="Senha do Usuário"
+            value={password}
+            onChange={(event) => {setPassword(event.target.value)}}    
+        />        
+        <Button color="secondary"
+        className='login-element' variant="contained" onClick={()=> handleLoginInButton()} >Entrar</Button>
+        
+    </form>
+    
     </>
 }
