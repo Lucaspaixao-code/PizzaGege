fbc95c40042aa71f0fbb50a03a1a7ca8ef6bebcb
Rafael da Silva Castro <rafaeldasilvacastroti@gmail.com>
2023-06-05 21:40:17 -0300

    Realizando ajustes
    Ajustando layout da tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Button, TextField } from "@mui/material";
 import useAuth from "../../router/validations/Auth/hook/useAuth";
 import { useNavigate } from "react-router-dom";
 import './index.css'
+import Logo from '../../core/components/Header/components/Logo'
+
 
 export default function Login(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
 
     const {handleLogin} = useAuth()
     const navigate = useNavigate()
@@ -15,10 +18,13 @@ export default function Login(){
         navigate("/")
     }
     return <>
-    <h1>ISSO È UM TESTE :D</h1>
-    <form id="login-form">    
+    <form id="login-form"> 
+        <img src="https://static.vecteezy.com/ti/vetor-gratis/t2/6886615-pizza-logo-cartoon-este-logotipo-e-altamente-adequado-para-qualquer-pizza-relacionado-restaurante-entrega-fast-food-bistro-catering-e-negocios-relacionados-comida-italiana-vetor.jpg" alt="" />
         <TextField
-            className='login-element'
+            className='login-element custom-textfield'
+            sx={{
+                margin: '10px',
+            }}
             type='text'
             id="usernameId"
             label="Nome do Usuário"
@@ -26,15 +32,25 @@ export default function Login(){
             onChange={(event) => {setUsername(event.target.value)}}    
         /> 
         <TextField
-            className='login-element'
+            className='login-element custom-textfield'
+            sx={{
+                margin: '10px',
+            }}            
             type='text'
             id="passwordId"
             label="Senha do Usuário"
             value={password}
             onChange={(event) => {setPassword(event.target.value)}}    
         />        
-        <Button color="secondary"
-        className='login-element' variant="contained" onClick={()=> handleLoginInButton()} >Entrar</Button>
+        <Button color="primary"
+        className='login-element' 
+        sx={{
+            padding:'15px',
+            width:'85%',
+            marginTop:'100px'
+        }}
+        variant="contained" 
+        onClick={()=> handleLoginInButton()} >Entrar</Button>
         
     </form>
     
